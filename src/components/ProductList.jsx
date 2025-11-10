c8f76faf66be5c21713829c5c84dcc4743d7c476
TroelsAtWork <tras@kea.dk>
2025-11-10 14:56:59 +0100

    Add changes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const ProductList = () => {
   return (
-    <Suspense>
+    <Suspense fallback={<div>Loading products...</div>}>
       <FetchProduct />
     </Suspense>
   );
