fdcdeebbe82c626e72c69c9108b7b4220928c89e
TroelsAtWork <tras@kea.dk>
2025-11-10 15:18:20 +0100

    Add changes

--- src/app/(pages)/details/[id]/page.js
+++ src/app/(pages)/details/[id]/page.js
@@ -1,21 +1,23 @@
 import Image from "next/image";
 import { Suspense } from "react";
 
-export default async function details({ params }) {
+export default function details({ params }) {
+  return <FetchProduct params={params} />;
+}
+
+async function FetchProduct({ params }) {
   const { id } = await params;
   const response = await fetch(`https://dummyjson.com/products/${id}`);
   const product = await response.json();
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex justify-center">
-        <Image
-          loading="eager"
-          alt={product.brand ? product.brand : "Product Image"}
-          src={product.thumbnail}
-          width={300}
-          height={200}
-        />
-      </div>
-    </Suspense>
+    <div className="flex justify-center">
+      <Image
+        loading="eager"
+        alt={product.brand ? product.brand : "Product Image"}
+        src={product.thumbnail}
+        width={300}
+        height={200}
+      />
+    </div>
   );
 }
